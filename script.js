a54e3bdf875a4c0698ca37da9872c7a059721e5b
BeldyAdam <beldy.adam@gmail.com>
2023-03-24 13:47:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+import { ADATLISTA } from "./adat.js"
+
+$(function(){
+
+
+})
+
+function osszeallit(lista){
+    
+}
+
+
+
+
+function kereses(){
+/*     $(`#nev`) */
+    /* const searchBar = document.getElementById(""); */ /* jQuery */
+}
